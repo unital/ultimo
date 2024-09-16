a3cec0a19f922df49f5c1240c7dae88002b03b3d
Corran Webster <cwebster@unital.dev>
2024-09-16 09:36:39 +0100

    Make tests more verbose.

--- ci/test.py
+++ ci/test.py
@@ -14,16 +14,18 @@ import click
 @click.command()
 def test():
     """Run unit tests in micropython"""
+    print("Running Tests")
     failures = []
     test_dir = Path("tests/ultimo")
     os.environ["MICROPYPATH"] = "src:" + os.environ.get('MICROPYPATH', ":.frozen:~/.micropython/lib:/usr/lib/micropython")
     for path in sorted(test_dir.glob("*.py")):
+        print(path.name, "... ", end="", flush=True)
         result = run_test(path)
         if result:
             failures.append(result)
-            print('F', end='', flush=True)
+            print('FAILED')
         else:
-            print('.', end='', flush=True)
+            print('OK')
     print()
 
     for path, stdout, stderr in failures:
@@ -37,6 +39,8 @@ def test():
 
     if failures:
         sys.exit(1)
+    else:
+        print("PASSED")
 
 
 def run_test(path):
