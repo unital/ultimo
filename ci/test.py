b331c8da423ce9bc6b6a1e0dc45501a2614c6729
Corran Webster <cwebster@unital.dev>
2024-09-16 09:23:39 +0100

    Add github action to run unit tests on every PR.

---
+++ ci/test.py
@@ -0,0 +1,49 @@
+# SPDX-FileCopyrightText: 2024-present Unital Software <info@unital.dev>
+#
+# SPDX-License-Identifier: MIT
+
+from pathlib import Path
+import os
+import subprocess
+import sys
+
+import click
+
+
+
+@click.command()
+def test():
+    """Run unit tests in micropython"""
+    failures = []
+    test_dir = Path("tests/ultimo")
+    os.environ["MICROPYPATH"] = "src:" + os.environ.get('MICROPYPATH', ":.frozen:~/.micropython/lib:/usr/lib/micropython")
+    for path in sorted(test_dir.glob("*.py")):
+        result = run_test(path)
+        if result:
+            failures.append(result)
+            print('F', end='', flush=True)
+        else:
+            print('.', end='', flush=True)
+    print()
+
+    for path, stdout, stderr in failures:
+        print("FAILURE: ", path.name)
+        print("STDOUT ", "="*70)
+        print(stdout.decode('utf-8'))
+        print()
+        print("STDERR ", "="*70)
+        print(stderr.decode('utf-8'))
+        print()
+
+    if failures:
+        sys.exit(1)
+
+
+def run_test(path):
+    try:
+        subprocess.run(["micropython", path], capture_output=True, check=True)
+    except subprocess.CalledProcessError as exc:
+        return (path, exc.stdout, exc.stderr)
+
+if __name__ == "__main__":
+    test()
